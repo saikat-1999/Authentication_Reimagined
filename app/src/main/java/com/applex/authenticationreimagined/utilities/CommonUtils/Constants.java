e32c02753ac521555107afb03f68a615d890c151
Madhurima <madhumukherjee1999@gmail.com>
2021-10-29 02:36:43 +0530

    Hii

--- app/src/main/java/com/applex/authenticationreimagined/utilities/CommonUtils/Constants.java
+++ app/src/main/java/com/applex/authenticationreimagined/utilities/CommonUtils/Constants.java
@@ -9,5 +9,10 @@ public class Constants {
     public static final String IS_FIRST_TIME = "firsttime";
     public static final String PROPERTY_CURRENT_USER = "property_user";
     public static final String SHARECODE = "sharecode";
+    public static final String[] READ_WRITE_CAMERA_PERMISSIONS = {
+            Manifest.permission.WRITE_EXTERNAL_STORAGE,
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.CAMERA
+    };
 
 }
